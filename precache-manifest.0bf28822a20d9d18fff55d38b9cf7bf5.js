fabff54f36906f9f3c4e98e985552ee25dd6596c
Amit Erez <amiterez98@gmail.com>
2020-03-15 15:21:47 +0200

    Updates

---
+++ precache-manifest.0bf28822a20d9d18fff55d38b9cf7bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db30bc71c378d3409acb3eaf89e16f6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1cd43e971453ed725b10",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "115dc875450fd0d13be1",
+    "url": "/robofriends/static/css/main.c8fb97e9.chunk.css"
+  },
+  {
+    "revision": "1cd43e971453ed725b10",
+    "url": "/robofriends/static/js/2.b425e1ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.b425e1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115dc875450fd0d13be1",
+    "url": "/robofriends/static/js/main.c19144a9.chunk.js"
+  },
+  {
+    "revision": "d61752120ccc8edc3e2d",
+    "url": "/robofriends/static/js/runtime-main.c231979a.js"
+  }
+]);
